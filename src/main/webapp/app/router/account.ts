cced2f03bd38a013e85efb00a875ac86cd2a80c1
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-12-22 22:33:51 +0100

    Remove reset password

--- src/main/webapp/app/router/account.ts
+++ src/main/webapp/app/router/account.ts
@@ -1,8 +1,6 @@
 import { Authority } from '@/shared/security/authority';
 
 const Register = () => import('@/account/register/register.vue');
-const ResetPasswordInit = () => import('@/account/reset-password/init/reset-password-init.vue');
-const ResetPasswordFinish = () => import('@/account/reset-password/finish/reset-password-finish.vue');
 const Settings = () => import('@/account/settings/settings.vue');
 
 export default [
@@ -10,16 +8,6 @@ export default [
     path: '/register',
     name: 'Register',
     component: Register,
-  },
-  {
-    path: '/account/reset/request',
-    name: 'ResetPasswordInit',
-    component: ResetPasswordInit,
-  },
-  {
-    path: '/account/reset/finish',
-    name: 'ResetPasswordFinish',
-    component: ResetPasswordFinish,
   },
   {
     path: '/account/settings',
