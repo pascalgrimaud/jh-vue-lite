bb5cd1a4cda4af31203007733fd1df89969bb6df
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-12-22 23:58:27 +0100

    increase coverage in jest conf

--- src/test/javascript/jest.conf.js
+++ src/test/javascript/jest.conf.js
@@ -39,8 +39,8 @@ module.exports = {
   coverageThreshold: {
     global: {
       statements: 80,
-      branches: 60,
-      functions: 70,
+      branches: 70,
+      functions: 80,
       lines: 80,
     },
   },
